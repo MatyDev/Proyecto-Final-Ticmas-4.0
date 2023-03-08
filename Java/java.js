73819f69388c5050b6258e6a7235da8a9e7da2d4
Matías Herrera <RocioPerdomo>
2023-03-08 20:04:19 -0300

    Subida del proyecto

---
+++ Java/java.js
@@ -0,0 +1,24 @@
+    document.getElementById('nombre').addEventListener('mouseover', function()
+    {
+        document.getElementById('texto').innerHTML="Herrera Matías Adriel";
+    });
+    
+    document.getElementById('email').addEventListener('mouseover', function()
+    {
+        document.getElementById('texto').innerHTML="soyadrielwey@gmail.com";
+    });
+    
+    document.getElementById('fecha').addEventListener('mouseover', function()
+    {
+        document.getElementById('texto').innerHTML="Since 1996";
+    });
+    
+    document.getElementById('dir').addEventListener ('mouseover', function()
+    {
+        document.getElementById('texto').innerHTML="Liberty City 123";
+    });
+    
+    document.getElementById('cel').addEventListener ('mouseover', function()
+    {
+        document.getElementById('texto').innerHTML="+5491125576427";
+    });
